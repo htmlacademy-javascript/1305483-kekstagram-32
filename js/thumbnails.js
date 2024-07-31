eb8bae755f9c69a25adf00d76a1e02440900a83e
Артем Шеховцов <freemanvrn@gmail.com>
2024-07-31 06:40:56 +0300

    Thumbnail fix

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -2,20 +2,20 @@ const picturesContainer = document.querySelector('.pictures');
 const templateElement = document.querySelector('#picture').content.querySelector('.picture');
 
 const createThumbnail = (picture) => {
-  const element = templateElement.cloneNode(true);
-  const image = element.querySelector('.picture__img');
+  const thumbnail = templateElement.cloneNode(true);
+  const image = thumbnail.querySelector('.picture__img');
   image.src = picture.url;
   image.alt = picture.description;
-  element.querySelector('.picture__likes').textContent = picture.likes;
-  element.querySelector('.picture__comments').textContent = picture.comments.length;
+  thumbnail.querySelector('.picture__likes').textContent = picture.likes;
+  thumbnail.querySelector('.picture__comments').textContent = picture.comments.length;
   return element;
 };
 
 const makeThumbnails = (pictures) => {
   const fragment = document.createDocumentFragment();
   pictures.forEach((picture) => {
-    const element = createThumbnail(picture);
-    fragment.append(element);
+    const thumbnail = createThumbnail(picture);
+    fragment.append(thumbnail);
   });
   picturesContainer.append(fragment);
 };
